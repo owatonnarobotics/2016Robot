4c8084e15db596563dddad6c426a917e4480d9bf
roshanpoduval <roshan.poduval@gmail.com>
2016-01-15 15:50:10 -0600

    Put in controller sensitivity

--- src/org/usfirst/frc/team4624/robot/RobotMap.java
+++ src/org/usfirst/frc/team4624/robot/RobotMap.java
@@ -6,10 +6,11 @@ package org.usfirst.frc.team4624.robot;
  * floating around.
  */
 public class RobotMap {
+	
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
+    public static final int leftMotor = 0;
+    public static final int rightMotor = 1;
     
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
